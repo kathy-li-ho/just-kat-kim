a3593c8c705f726ec4118c600f9c6ee81383246c
Kathy-Li <li@work.co>
2019-08-26 13:46:10 -0400

    fix window error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import {useStaticQuery, graphql} from 'gatsby'
+import React, {useEffect} from 'react';
+import {useStaticQuery, graphql} from 'gatsby';
 import Social from './social/Social';
-import './Footer.css'
+import './Footer.css';
 
-const transparentPNG = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
-const path = obj => {return  obj && "node" in obj ? obj.node.fluid : {src: transparentPNG}};
+const transparentPNG =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+const path = obj => {
+    return obj && 'node' in obj ? obj.node.fluid : {src: transparentPNG};
+};
 const generateSizes = () => {
-    const bps = [576, 768, 992, 1200, 1600]
+    const bps = [576, 768, 992, 1200, 1600];
     const sizes = bps.map((bp, i) => {
-        return `${i ? ' \n' : ''}(max-width: ${bp - 1}px) ${bp}px`
-    })
-    sizes.push('\n1920px')
+        return `${i ? ' \n' : ''}(max-width: ${bp - 1}px) ${bp}px`;
+    });
+    sizes.push('\n1920px');
     return {sizes: sizes.toString()};
-}
+};
 
 const Footer = () => {
     const footers = useStaticQuery(graphql`
         query {
-            allImageSharp(filter: {fluid: {originalName: {regex: "/footer/"}}}) {
+            allImageSharp(
+                filter: {fluid: {originalName: {regex: "/footer/"}}}
+            ) {
                 edges {
                     node {
-                        fluid(srcSetBreakpoints: [576, 768, 992, 1200, 1600], maxWidth: 1920) {
+                        fluid(
+                            srcSetBreakpoints: [576, 768, 992, 1200, 1600]
+                            maxWidth: 1920
+                        ) {
                             src
                             srcSet
                         }
@@ -30,12 +38,19 @@ const Footer = () => {
         }
     `).allImageSharp.edges;
 
-    let slug = window.location.pathname;
+    let slug = '/';
+
+    useEffect(() => {
+        slug = window && window.location.pathname;
+    });
+
     if (slug === '/') slug = '/home/';
-    const page = slug.slice(1,-1);
-    const {src, srcSet} = path(footers.filter(edge => path(edge).src.includes(page))[0]);
+    const page = slug.slice(1, -1);
+    const {src, srcSet} = path(
+        footers.filter(edge => path(edge).src.includes(page))[0]
+    );
     const imageProps = {
-        className: "footerImg",
+        className: 'footerImg',
         src,
         srcSet,
         ...generateSizes(),
@@ -49,7 +64,7 @@ const Footer = () => {
             </small>
             <img {...imageProps} alt="footer" />
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
