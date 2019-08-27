6f0475284bd23894d949e9d328c5ac8a049ae021
Kathy-Li <li@work.co>
2019-08-27 17:10:48 -0400

    favorites

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
+import {useStaticQuery, graphql} from 'gatsby';
 import Layout from '../components/layout/Layout';
 
-const FavoritesPage = () => (
+const FavoritesPage = () => {
+    const contentHTML = useStaticQuery(
+        graphql`
+            query {
+                allDropboxPaperDocument(
+                    filter: {id: {eq: "1hmiDz44GUuVi0PuZTcRk"}}
+                ) {
+                    edges {
+                        node {
+                            content
+                        }
+                    }
+                }
+            }
+        `,
+        []
+    ).allDropboxPaperDocument.edges[0].node.content;
+
+    const contentNoLinkStyles = contentHTML.replace(
+        /style="color: #47B5FA; text-decoration: none"/g,
+        ''
+    );
+
+    useLayoutEffect(() => {
+        const mainElement = document.querySelector('#list');
+        if (!mainElement.innerHTML) {
+            mainElement.insertAdjacentHTML('afterbegin', contentNoLinkStyles);
+        }
+    });
+
+    return (
     <Layout page="favorites" pageTitle="Kat's Bag">
-        content
+        <div id="list" />
     </Layout>
-);
+    );
+};
 
 export default FavoritesPage;
