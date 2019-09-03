60d5ed4d0fdfe46bec83f3a269c6e7394b0936ba
Kathy-Li <li@work.co>
2019-09-03 10:45:36 -0400

    center and style image grid

--- src/utils.js
+++ src/utils.js
@@ -6,16 +6,32 @@ export const getValueIn = (context, ...path) => {
     return value;
 };
 
+const documentTitleRegex =
+    '(<div dir="auto" style="font-weight: 400;.*?</div>)';
+const excessAttrRegex = '(dir="auto" style="line-height: 26px;")';
+const linkStylesRegex = '(style="color: #47B5FA; text-decoration: none")';
+const codeRegex =
+    '(<div dir="auto" style="line-height: 26px;" class="ace-line "><code.*?</div>)';
+const joinedRegex = [
+    documentTitleRegex,
+    codeRegex,
+    linkStylesRegex,
+    excessAttrRegex,
+].join('|');
+const removeRegex = new RegExp(joinedRegex, 'g');
+const cssMaxHeightRegex = new RegExp('max-height: [0-9]{1,}px', 'g');
+const cssHeightAuto = 'height: auto';
+
 export const cleanDropboxHTML = dbstr => {
-    let cleanString = dbstr;
-    cleanString = dbstr.replace(
-        /(<div dir="auto" style="font-weight: 400;.*?<\/div>)|(dir="auto" style="line-height: 26px;")|(style="color: #47B5FA; text-decoration: none")/g,
-        ''
-    );
-    return cleanString;
+    const cleanString = dbstr.replace(removeRegex, '');
+    const finalString = cleanString.replace(cssMaxHeightRegex, cssHeightAuto);
+    return finalString;
 };
 
-export const insertContent = (htmlString, parentNode = document.querySelector('main')) => {
+export const insertContent = (
+    htmlString,
+    parentNode = document.querySelector('main')
+) => {
     if (parentNode && !parentNode.innerHTML) {
         parentNode.insertAdjacentHTML('afterbegin', htmlString);
     }
