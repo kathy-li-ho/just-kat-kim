3bafe8a7aeb3df333ce890319b4430a4caf6b7a8
Kathy Li <li@work.co>
2021-10-12 17:46:11 -0400

    back to cloudinary

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import {TinaCloudCloudinaryMediaStore} from 'next-tinacms-cloudinary';
 
 const TinaCMS = dynamic(() => import('tinacms'), {ssr: false});
 
-const NEXT_PUBLIC_TINA_CLIENT_ID = process.env.NEXT_PUBLIC_TINA_CLIENT_ID;
+const {NEXT_PUBLIC_BASE_BRANCH, NEXT_PUBLIC_TINA_CLIENT_ID} = process.env;
 const NEXT_PUBLIC_USE_LOCAL_CLIENT =
     process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT || 0;
 
